3cac0f173b8cd27acd89e65a9bd6df06e6b40283
Duncan Boan <djboan@gmail.com>
2018-06-14 10:01:47 -0700

    further contact form changes

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import Form from '../components/Form';
-
 const Contact = styled.div`
   background-color: #ffffcc;
   border-top-right-radius: 10px;
@@ -15,7 +13,46 @@ const Contact = styled.div`
 export default () => {
   return (
     <Contact>
-      <Form />
+      <form
+        action="/success"
+        name="contact"
+        method="post"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+      >
+        <div>
+          <label htmlFor="name">Name </label>
+          <input type="text" name="name" id="name" />
+        </div>
+        <div>
+          <label htmlFor="email">Email </label>
+          <input type="text" name="email" id="email" />
+        </div>
+        <div>
+          <label htmlFor="message">Message</label>
+          <textarea
+            name="message"
+            id="message"
+            placeholder="Please include your contact phone number in your message"
+          />
+        </div>
+        <input type="hidden" name="bot-field" />
+
+        <div className="actions">
+          <div>
+            <input type="submit" value="Send Message" className="special" />
+          </div>
+          <div>
+            <input type="reset" value="Clear Form" />
+          </div>
+          <div id="security">
+            <p>
+              Please Note: your message will be sent securely, and I will not
+              share it with anyone without your consent.{' '}
+            </p>{' '}
+          </div>
+        </div>
+      </form>
     </Contact>
   );
 };
